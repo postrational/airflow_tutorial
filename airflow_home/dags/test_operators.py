cb9b6b90e578d514439255a425ee42f181d33ccb
Michal Karzyński <bitbucket@karzyn.com>
2017-03-20 23:19:41 +0100

    Your first Airflow Sensor

--- airflow_home/dags/test_operators.py
+++ airflow_home/dags/test_operators.py
@@ -1,7 +1,8 @@
 from datetime import datetime
 from airflow import DAG
 from airflow.operators.dummy_operator import DummyOperator
-from airflow.operators import MyFirstOperator
+from airflow.operators import MyFirstOperator, MyFirstSensor
+
 
 dag = DAG('my_test_dag', description='Another tutorial DAG',
           schedule_interval='0 12 * * *',
@@ -9,7 +10,9 @@ dag = DAG('my_test_dag', description='Another tutorial DAG',
 
 dummy_task = DummyOperator(task_id='dummy_task', dag=dag)
 
+sensor_task = MyFirstSensor(task_id='my_sensor_task', poke_interval=30, dag=dag)
+
 operator_task = MyFirstOperator(my_operator_param='This is a test.',
                                 task_id='my_first_operator_task', dag=dag)
 
-dummy_task >> operator_task
+dummy_task >> sensor_task >> operator_task
--- airflow_home/plugins/my_operators.py
+++ airflow_home/plugins/my_operators.py
@@ -1,10 +1,12 @@
 import logging
-
+from datetime import datetime
 from airflow.models import BaseOperator
 from airflow.plugins_manager import AirflowPlugin
 from airflow.utils.decorators import apply_defaults
+from airflow.operators.sensors import BaseSensorOperator
 
 log = logging.getLogger(__name__)
+
 
 class MyFirstOperator(BaseOperator):
 
@@ -15,11 +17,27 @@ class MyFirstOperator(BaseOperator):
 
     def execute(self, context):
         log.info("Hello World!")
-
-        from IPython import embed; embed()
-
         log.info('operator_param: %s', self.operator_param)
+
+
+class MyFirstSensor(BaseSensorOperator):
+    template_fields = tuple()
+    ui_color = '#b5f2ff'
+
+    @apply_defaults
+    def __init__(self, *args, **kwargs):
+        super(MyFirstSensor, self).__init__(*args, **kwargs)
+
+    def poke(self, context):
+        current_minute = datetime.now().minute
+        if current_minute % 3 != 0:
+            log.info("Current minute (%s) not is divisible by 3, sensor will retry.", current_minute)
+            return False
+
+        log.info("Current minute (%s) is divisible by 3, sensor finishing.", current_minute)
+        return True
+
 
 class MyFirstPlugin(AirflowPlugin):
     name = "my_first_plugin"
-    operators = [MyFirstOperator]
+    operators = [MyFirstOperator, MyFirstSensor]
