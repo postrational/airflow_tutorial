fc918909763eba0a1671ecda4629b4ffec45c441
Michal Karzyński <bitbucket@karzyn.com>
2017-03-20 23:04:46 +0100

    Your first Airflow Operator

---
+++ airflow_home/dags/test_operators.py
@@ -0,0 +1,15 @@
+from datetime import datetime
+from airflow import DAG
+from airflow.operators.dummy_operator import DummyOperator
+from airflow.operators import MyFirstOperator
+
+dag = DAG('my_test_dag', description='Another tutorial DAG',
+          schedule_interval='0 12 * * *',
+          start_date=datetime(2017, 3, 20), catchup=False)
+
+dummy_task = DummyOperator(task_id='dummy_task', dag=dag)
+
+operator_task = MyFirstOperator(my_operator_param='This is a test.',
+                                task_id='my_first_operator_task', dag=dag)
+
+dummy_task >> operator_task
